f65c1371a945a7549859b76c51a8ed892dea64f1
Francisco Villa <fvillag01@educantabria.es>
2021-04-21 00:10:06 +0200

    estilos pagina principal

--- index.php
+++ index.php
@@ -11,13 +11,15 @@
         
         <section class="index-section">
             <!-- <h4>El portal de empleo para recien titulados de FP</h4> -->
-            <div class="index-section_item"><a href="login.php?tipo=empresa">EMPRESAS</a></div>
-            <div class="index-section_item"><a href="login.php?tipo=titulado">TITULADOS</a></div>
-            <div class="index-section_item"><a href="empleos.php">OFERTAS DE EMPLEOS</a></div>
+            <div class="index-section_item"><a href="login.php?tipo=empresa"><i class="fas fa-building"></i> EMPRESAS</a></div>
+            <div class="index-section_item"><a href="login.php?tipo=titulado"><i class="fas fa-graduation-cap"></i> TITULADOS</a></div>
+            <div class="index-section_item"><a href="empleos.php"><i class="fas fa-briefcase"></i> OFERTAS DE EMPLEOS</a></div>
         </section>
         <footer>
-        
+            <small>Página realizada por Fr@ncisc@ Vill@</small>
+            
         </footer>
+        <div class="imagenfondo"></div>
     </div>
     
 
