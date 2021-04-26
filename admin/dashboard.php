6bef3ac173f0463ae17c7c86b57b8c2e0ef8a371
Francisco Villa <francisco.villa@itecan.es>
2021-04-26 15:55:41 +0200

    estilos del sidebar del dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -25,10 +25,16 @@
         </footer>
   </div> -->
   <div class="grid-container">
+        <div class="menu-icon">
+            <i class="fas fa-bars"></i>
+        </div>
         <header class="header index-header">
             <?php include 'inc/header.php' ?>
         </header>
-        <aside class="sidenav">sidebar</aside>
+        <aside class="sidenav"><div class="sidenav__close-icon">
+            <i class="fas fa-times"></i>
+            </div>
+        </aside>
         <main class="main">main</main>
         <footer class="footer">footer</footer>
     </div>
